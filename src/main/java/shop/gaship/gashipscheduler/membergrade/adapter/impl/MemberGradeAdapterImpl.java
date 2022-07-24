aa108fd6dea433995264008c956e6a261b51ca91
semi <semikimm.m@gmail.com>
2022-07-24 22:34:05 +0900

    [feat✨] : 회원등급 adapter 전체조회 추가

---
+++ src/main/java/shop/gaship/gashipscheduler/membergrade/adapter/impl/MemberGradeAdapterImpl.java
@@ -0,0 +1,44 @@
+package shop.gaship.gashipscheduler.membergrade.adapter.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import shop.gaship.gashipscheduler.config.ServerConfig;
+import shop.gaship.gashipscheduler.membergrade.adapter.MemberGradeAdapter;
+import shop.gaship.gashipscheduler.membergrade.dto.response.MemberGradeResponseDto;
+
+/**
+ * MemberGradeAdapter interface 구현체.
+ *
+ * @author : 김세미
+ * @see shop.gaship.gashipscheduler.membergrade.adapter.MemberGradeAdapter
+ * @since 1.0
+ */
+@Component
+@RequiredArgsConstructor
+public class MemberGradeAdapterImpl implements MemberGradeAdapter {
+
+    private final ServerConfig serverConfig;
+    private static final String MEMBER_GRADE_RUL = "/api/member-grades";
+
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl(serverConfig.getShoppingMallUrl())
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+
+    @Override
+    public Flux<MemberGradeResponseDto> findMemberGrades() {
+        UriBuilder uriBuilder = UriComponentsBuilder.newInstance();
+
+        return webClient.get()
+                .uri(uriBuilder.path(MEMBER_GRADE_RUL)
+                        .build())
+                .retrieve()
+                .bodyToFlux(MemberGradeResponseDto.class);
+    }
+}
