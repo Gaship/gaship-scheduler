d9b638f16ca7a340ead1903115b14f75e46bd3c7
semi <semikimm.m@gmail.com>
2022-07-28 16:05:18 +0900

    [refactor🔨] : job 실행관련 exception 처리 수정

--- src/main/java/shop/gaship/gashipscheduler/scheduler/gradeadvancement/GradeAdvancementScheduler.java
+++ src/main/java/shop/gaship/gashipscheduler/scheduler/gradeadvancement/GradeAdvancementScheduler.java
@@ -3,10 +3,15 @@ package shop.gaship.gashipscheduler.scheduler.gradeadvancement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import shop.gaship.gashipscheduler.scheduler.gradeadvancement.config.GradeAdvancementConfig;
+import shop.gaship.gashipscheduler.scheduler.gradeadvancement.config.GradeAdvancementJobConfig;
+import shop.gaship.gashipscheduler.scheduler.gradeadvancement.exception.MemberGradeAdvanceRunException;
 
 /**
  * 회원승급 job scheduler.
@@ -18,18 +23,24 @@ import shop.gaship.gashipscheduler.scheduler.gradeadvancement.config.GradeAdvanc
 @Component
 public class GradeAdvancementScheduler {
     private final JobLauncher jobLauncher;
-    private final GradeAdvancementConfig gradeAdvancementConfig;
+    private final GradeAdvancementJobConfig gradeAdvancementJobConfig;
+
 
     /**
      * 매일 밤 자정마다 실행되는 회원승급 job 실행 메서드.
      *
-     * @throws Exception job 실행 관련 exception.
-     * @author 김세미
+     * @throws MemberGradeAdvanceRunException 회원승급 작업 실행시 발생할 수 있는 exception.
      */
-    @Scheduled(cron = "0 0 24 * * ?")
-    public void doGradeAdvancement() throws Exception {
-        Job job = gradeAdvancementConfig.advanceJob();
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void doGradeAdvancement() {
+        Job job = gradeAdvancementJobConfig.advanceJob();
         JobParameters jobParameters = new JobParameters();
-        jobLauncher.run(job, jobParameters);
+
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
+                 | JobParametersInvalidException | JobRestartException e) {
+            throw new MemberGradeAdvanceRunException();
+        }
     }
 }
